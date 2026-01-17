87bcaf32a0aa59de74c35cef4b2d0af3b631dc9f
storm <1910287579@qq.com>
2026-01-17 10:44:16 +0800

    苍穹外卖员分类相关接口业务代码开发

---
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -0,0 +1,23 @@
+package com.sky.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.github.pagehelper.Page;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+
+@Mapper
+public interface CategoryMapper extends BaseMapper<Category> {
+
+
+
+
+    Page<Category> pageQuery(CategoryPageQueryDTO categoryPageQueryDTO);
+
+    @Select("select * from category where type = #{type} order by sort asc")
+    List<Category> list(Integer type);
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,80 @@
+package com.sky.service.impl;
+
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.sky.context.BaseContext;
+import com.sky.dto.CategoryDTO;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import com.sky.mapper.CategoryMapper;
+import com.sky.result.PageResult;
+import com.sky.service.CategoryService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    CategoryMapper categoryMapper;
+
+    @Override
+    public PageResult pageQuery(CategoryPageQueryDTO categoryPageQueryDTO) {
+        PageHelper.startPage(categoryPageQueryDTO.getPage(), categoryPageQueryDTO.getPageSize());
+
+        Page<Category> page= categoryMapper.pageQuery(categoryPageQueryDTO);
+
+        long total=page.getTotal();
+        List records=page.getResult();
+
+        return new PageResult( total,records);
+    }
+
+    @Override
+    public void add(CategoryDTO categoryDTO) {
+        Category category=new Category();
+        BeanUtils.copyProperties(categoryDTO,category);
+
+        category.setCreateTime(LocalDateTime.now());
+        category.setUpdateTime(LocalDateTime.now());
+        category.setStatus(1);
+
+        category.setCreateUser(BaseContext.getCurrentId());
+        category.setUpdateUser(BaseContext.getCurrentId());
+
+        categoryMapper.insert(category);
+
+    }
+
+    @Override
+    public void startOrStop(Integer status, Long id) {
+        Category category=new Category();
+        category.setStatus(status);
+        category.setId(id);
+        categoryMapper.updateById( category);
+    }
+
+    @Override
+    public List<Category> list(Integer type) {
+        return categoryMapper.list(type);
+    }
+
+    @Override
+    public void delete(Long id) {
+        categoryMapper.deleteById(id);
+    }
+
+    @Override
+    public void update(CategoryDTO categoryDTO) {
+        Category category=new Category();
+        BeanUtils.copyProperties(categoryDTO,category);
+        category.setUpdateTime(LocalDateTime.now());
+        category.setUpdateUser(BaseContext.getCurrentId());
+        categoryMapper.updateById(category);
+
+    }
+}
